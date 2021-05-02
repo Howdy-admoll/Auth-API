8d82e9c001eb98d05fea1690ae84739178a5ff9a
Ad <83498835+Howdy-admoll@users.noreply.github.com>
2021-05-02 02:06:33 +0100

    update main.go

--- main.go
+++ main.go
@@ -4,3 +4,16 @@ import (
   "net/http"
   "github.com/gin-gonic/gin"
 )
+func PostMethod(c *gin.Context) {
+  fmt.Println("\napi.go 'PostMethod' called")
+  message := "PostMethod called"
+  c.JSON(http.StatusOK, message)
+}
+func GetMethod(c *gin.Context) {
+  fmt.Println("\napi.go 'GetMethod' called")
+  message := "GetMethod called"
+  c.JSON(http.StatusOK, message)
+}
+func main() {
+  router := gin.Default()
+}
