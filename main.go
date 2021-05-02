059439e2ef07b0f7daf1b7a3af2b792e071d427a
Ad <83498835+Howdy-admoll@users.noreply.github.com>
2021-05-02 02:25:24 +0100

    update main.go

--- main.go
+++ main.go
@@ -4,21 +4,26 @@ import (
   "net/http"
   "github.com/gin-gonic/gin"
 )
-func PostMethod(c *gin.Context) {
-  fmt.Println("\napi.go 'PostMethod' called")
-  message := "PostMethod called"
-  c.JSON(http.StatusOK, message)
-}
 func GetMethod(c *gin.Context) {
-  fmt.Println("\napi.go 'GetMethod' called")
-  message := "GetMethod called"
+  fmt.Println("\n'GetMethod' called")
+  IdValue := c.Params.ByName("IdValue")
+  message := "GetMethod Called With Param: " + IdValue
   c.JSON(http.StatusOK, message)
+ReqPayload := make([]byte, 1024)
+  ReqPayload, err := c.GetRawData()
+  if err != nil {
+        fmt.Println(err)
+        return
+  }
+  fmt.Println("Request Payload Data: ", string(ReqPayload))
 }
 func main() {
   router := gin.Default()
+subRouterAuthenticated := router.Group("/api/v1/PersonId", gin.BasicAuth(gin.Accounts{
+    "admin": "adminpass",
+  }))
+subRouterAuthenticated.GET("/:IdValue", GetMethod)
+listenPort := "1357"
   
-  router.POST("/", PostMethod)
-  router.GET("/", GetMethod)
- listenPort := "4000"
-router.Run(":"+listenPort)
+  router.Run(":"+listenPort)
 }
